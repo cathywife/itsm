3b5ad2c44b53a26324c2bc54a285cee21d8d5ac3
liang <liang78672055@live.com>
2017-02-01 19:30:49 +0800

    20170201

--- itsm/ldtk/models.py
+++ itsm/ldtk/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
 # Create your models here.
+class itsmsj(models.Model):
+	sjid=models.IntegerField(default=0)
+	
+class user_group(models.Model):
+	ug_name=models.CharField(max_length=100)
+
+class itsmzsk(models.Model):
+		zsk_name=models.CharField(max_length=200)
+		
+
+class itsmwt(models.Model):
+		wt_name=models.CharField(max_length=100)
+		
+
+class yonghudanwei(models.Model):
+		yhdw_name=models.CharField(max_length=100)
+		yhdw_address=models.CharField(max_length=100)
+		yhdw_Tel=models.IntegerField()
+		
+		
+
+class yonghu(models.Model):
+		yh_name=models.CharField(max_length=50)
+		yh_psw=models.CharField(max_length=20)
+		yh_phone=models.IntegerField()
+		yh_address=models.CharField(max_length=100)
+		yh_dan=models.ForeignKey(yonghudanwei)
+		yh_descripition=models.CharField(max_length=100)
+		yh_birthday=models.DateTimeField()
+		
+
+class fuwufanwei(models.Model):
+		fwfw_name=models.CharField(max_length=100)
+
+
+class xjitem(models.Model):
+		xjitem_name=models.CharField(max_length=100)
+		xjitem_group=models.ForeignKey(fuwufanwei)
+		
+class shebeileixin(models.Model):
+		sblx_name=models.CharField(max_length=100)
+
+		
+class shebei(models.Model):
+		sb_lx=models.ForeignKey(shebeileixin)
+		sb_jcsj=models.DateTimeField()
+
+		
+class peizhiku(models.Model):
+		pz_iitem=models.ForeignKey(shebei)
+		pz_user = models.CharField(max_length=100)
+		pz_pwd  = models.CharField(max_length=100)
+	
+class weizhi(models.Model):
+        wz_shen=models.CharField(max_length=30)
+	
+class jifang(models.Model):
+		jf_danwei=models.ForeignKey(yonghudanwei)
+		jf_gly=models.ForeignKey(yonghu)
+		jf_weizhi=models.ForeignKey(weizhi)
+		
+class jigui(models.Model):
+		jg_jf=models.ForeignKey(jifang)
+		jg_weizhi=models.CharField(max_length=100)
+
+
--- itsm/ldtk/views.py
+++ itsm/ldtk/views.py
@@ -4,10 +4,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader,Context
+from django import forms
 
 
 # Create your views here.
+
+class UserForm(forms.Form):
+	name=forms.CharField()
+	pwd=forms.CharField()
+
 def index(req):
-    #t = loader.get_template('ldtk/index.html');
-    #c=Context({})
 	return render(req,'ldtk/index.html',{})
+def home(req):
+	if req.method =='POST':
+		form = UserForm(req.POST)
+		if form.is_valid():
+		
+			return HttpResponse('ok')
+		
+	else:
+		form=UserForm()
+
+	return render(req,'ldtk/home.html',{})
+def register(req):
+	if req.method =='POST':
+		form = UserForm(req.POST)
+		if form.is_valid():
+		
+			return HttpResponse('ok')
+		
+	else:
+		form=UserForm()
+	return render(req,'ldtk/register.html',{})
+
+	
+	
+	
+	
+def dashboard(req):
+	return render(req,'ldtk/dashboar.html',{})
