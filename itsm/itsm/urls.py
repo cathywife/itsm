b4c38cd595311b7614bbeeb033f98ee4be592389
LIANG BO <liang78672055@live.com>
2017-02-06 03:27:05 +0800

    loginok

--- itsm/itsm/urls.py
+++ itsm/itsm/urls.py
@@ -14,14 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+from django.contrib import admin,auth
 from ldtk  import  views
 from django.conf.urls.static import static
 
 
+
+
 urlpatterns = [
+    url(r'^login/$', auth.views.login, name='login'),
+    url(r'^logout/$', auth.views.logout, name='logout'),
+    url(r'^password_change/$', auth.views.password_change, name='password_change'),
+    url(r'^password_change/done/$', auth.views.password_change_done, name='password_change_done'),
+    url(r'^password_reset/$', auth.views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth.views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth.views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth.views.password_reset_complete, name='password_reset_complete'),
+    
     url(r'^admin/', admin.site.urls),
-    url(r'^index/', views.index),
+    url(r'^client/', views.client),
 	url(r'^home/', views.home),
 	url(r'^register/', views.register),
 	url(r'^dash/', views.dashboard),
